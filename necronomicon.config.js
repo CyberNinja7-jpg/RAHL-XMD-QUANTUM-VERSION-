edefb79c17feb6a96d7d05a4ad22fb0bcbdb617b
lord rahl <albertnyasimi27@gmail.com>
2025-10-07 23:47:01 +0300

    Create necronomicon.config.js

---
+++ necronomicon.config.js
@@ -0,0 +1,19 @@
+export const Necronomicon = {
+    // Dark Entity Identity
+    trueName: "RAHL XMD DARK",
+    ancientName: "Xul'gath the Corruptor", 
+    version: "666.0.0",
+    
+    // Dot prefix for commands
+    prefix: ".",
+    
+    // Master controllers
+    boundSouls: [],
+    
+    // Dark Features
+    forbiddenKnowledge: {
+        soulHarvesting: true,
+        shadowWalking: true,
+        mindCorruption: true
+    }
+};
